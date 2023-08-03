6788f990feb0058b5ce4e0c6badfe415cc5c37ac
dmitry.ivzhenko <dmitry.ivzhenko@gmail.com>
2023-08-03 15:30:03 +0300

    properties

--- src/main/kotlin/com/polarbookshop/catalogservice/HomeController.kt
+++ src/main/kotlin/com/polarbookshop/catalogservice/HomeController.kt
@@ -2,11 +2,10 @@ package com.polarbookshop.catalogservice
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Mono
 
 @RestController
 class HomeController {
 
   @GetMapping("/")
-  fun getGreeting(): Mono<String> = Mono.just("Welcome to the book catalog!")
+  fun getGreeting(): String = "Welcome to the book catalog!"
 }
