16a384511fb9aa652bfc8fa81e86c9f22b44f211
dmitry.ivzhenko <dmitry.ivzhenko@gmail.com>
2023-09-22 10:36:32 +0300

    jdbc test

--- src/test/kotlin/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.kt
+++ src/test/kotlin/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.kt
@@ -5,11 +5,13 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest(
   webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests(
   @Autowired
   private val webTestClient: WebTestClient
